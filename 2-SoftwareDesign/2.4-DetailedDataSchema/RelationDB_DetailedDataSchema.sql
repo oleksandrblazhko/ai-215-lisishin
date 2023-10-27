bab6a80a2038d4daa48d4f03bb0333ad64de7613
lisishindanil <lisishindanil@gmail.com>
2023-10-27 12:20:25 +0300

    lab6

--- 2-SoftwareDesign/2.4-DetailedDataSchema/RelationDB_DetailedDataSchema.sql
+++ 2-SoftwareDesign/2.4-DetailedDataSchema/RelationDB_DetailedDataSchema.sql
@@ -8,26 +8,27 @@ CREATE TABLE UserInformation (
 
 CREATE TABLE User (
     UserID INT PRIMARY KEY,
-    Login VARCHAR(20) CHECK (LENGTH(Login) < 20),
-    Password VARCHAR(20) CHECK (LENGTH(Password) < 20)
+    Login VARCHAR(20) CHECK (LENGTH(Login) < 20 AND Login ~ '^[a-zA-Z0-9]*$'),
+    Password VARCHAR(20) CHECK (LENGTH(Password) < 20 AND Password ~ '^[a-zA-Z0-9]*$')
 );
+
 
 CREATE TABLE HealthCenterEmployee (
     EmployeeID INT PRIMARY KEY,
     Name VARCHAR(255) NOT NULL,
     Age SMALLINT CHECK (Age >= 0 AND Age <= 99),
-    Position VARCHAR(255) CHECK (LENGTH(Position) > 5)
+    Position VARCHAR(255)
 );
 
 CREATE TABLE HealthQuestion (
     QuestionID INT PRIMARY KEY,
-    QuestionText TEXT CHECK (LENGTH(QuestionText) > 0 AND LENGTH(QuestionText) < 500),
+    QuestionText TEXT,
     Date DATE DEFAULT CURRENT_DATE
 );
 
 CREATE TABLE HealthAnswer (
     AnswerID INT PRIMARY KEY,
-    AnswerText TEXT CHECK (LENGTH(AnswerText) > 0 AND LENGTH(AnswerText) < 500),
+    AnswerText TEXT,
     Date DATE DEFAULT CURRENT_DATE
 );
 
@@ -35,6 +36,6 @@ CREATE TABLE HealthData (
     DataID INT PRIMARY KEY,
     UserID INT,
     Date DATE CHECK (Date <= CURRENT_DATE),
-    HealthDataText TEXT CHECK (LENGTH(HealthDataText) < 500),
+    HealthDataText TEXT,
     FOREIGN KEY (UserID) REFERENCES User(UserID)
 );
